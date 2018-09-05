7ce7b84ff39bf068fa3641045ed8e18b2967dd31
wincentrtz <wincentang3636@gmail.com>
2018-09-05 18:16:17 +0700

    Card Component

--- src/components/cardBody.jsx
+++ src/components/cardBody.jsx
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
+import config from "../config.json";
 
 class CardBody extends Component {
+  getDateName = d => {
+    const date = new Date(d);
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    return days[date.getDay()];
+  };
+
   render() {
-    return <h1>CardBody</h1>;
+    return (
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">
+            {this.getDateName(this.props.weather.applicable_date)}
+          </h5>
+          <hr />
+          <div className="row">
+            <div className="col-6">
+              <img
+                src={
+                  config.apiUrl +
+                  "/static/img/weather/png/64/" +
+                  this.props.weather.weather_state_abbr +
+                  ".png"
+                }
+              />
+            </div>
+            <div className="col-6 text-right">
+              <h6>{Math.ceil(this.props.weather.max_temp)}°</h6>
+              <h6>{Math.ceil(this.props.weather.min_temp)}°</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
