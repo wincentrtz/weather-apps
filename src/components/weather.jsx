b85a0f60d8e4c4e9298e6778639e66087aeb99fc
wincentrtz <wincentang3636@gmail.com>
2018-09-05 18:40:02 +0700

    Today's weather

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CardBody from "./cardBody";
 import { getWoeId, getWeather } from "../services/weatherServices";
+import config from "../config.json";
 
 class Weather extends Component {
   state = {
@@ -21,9 +22,43 @@ class Weather extends Component {
   }
 
   render() {
-    const { weathers } = this.state;
+    const { weathers, now_weather } = this.state;
     return (
       <div className="container-fluid">
+        <div className="row">
+          <div
+            className="col-4 offset-2 text-right"
+            style={{ alignSelf: "flex-end" }}
+          >
+            <h2>{Math.ceil(now_weather.the_temp)}°</h2>
+          </div>
+          <div className="col-4 text-left">
+            <img
+              style={{ width: "30%" }}
+              src={
+                config.apiUrl +
+                "/static/img/weather/png/" +
+                now_weather.weather_state_abbr +
+                ".png"
+              }
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4 offset-2 text-right">
+            {Math.ceil(now_weather.min_temp)}° /
+            {Math.ceil(now_weather.max_temp)}°
+          </div>
+          <div className="col-2 text-right">
+            {"Real Feel@" + Math.ceil(now_weather.humidity)}°
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <h2 className="text-center">{now_weather.weather_state_name}</h2>
+          </div>
+        </div>
+
         <div className="row">
           <div className="col-12 col-md-1" />
           {weathers.map(weather => (
