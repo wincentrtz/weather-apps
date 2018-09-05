c395846965e952edd1b6d45b6e1c446590e10837
wincentrtz <wincentang3636@gmail.com>
2018-09-05 18:03:01 +0700

    Get weather data  & map to card

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
 import CardBody from "./cardBody";
+import { getWoeId, getWeather } from "../services/weatherServices";
 
 class Weather extends Component {
-  state = {};
+  state = {
+    now_weather: "",
+    weathers: []
+  };
+
+  async componentDidMount() {
+    let { data } = await getWoeId();
+    const weather = await getWeather(data[0].woeid);
+    const nextDay = weather.data.consolidated_weather.filter(
+      w => weather.data.consolidated_weather.indexOf(w) !== 0
+    );
+    this.setState({
+      now_weather: weather.data.consolidated_weather[0],
+      weathers: nextDay
+    });
+  }
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col-1" />
-          <div className="col-2">
-            <CardBody />
-          </div>
+          {this.state.weathers.map(weather => (
+            <div className="col-2" key={weather.id}>
+              <CardBody weather={weather} />
+            </div>
+          ))}
         </div>
       </div>
     );
